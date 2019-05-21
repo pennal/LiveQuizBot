958eefebbf5088f7300793de8029691339e14ceb
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2019-05-21 13:04:19 +0200

    Added new dependencies

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 pip3 install -r requirements.txt
 python3 -m nltk.downloader stopwords 
 python3 -m nltk.downloader punkt
+python3 -m spacy download it_core_news_sm
 
 brew install tesseract
 brew install homebrew/cask/android-platform-tools
